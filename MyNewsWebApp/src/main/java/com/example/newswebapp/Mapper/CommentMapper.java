3c9da255a548b0fef6ee92411f58e82dcb9f4119
sayoorJengkol <62087838+baskoroaji@users.noreply.github.com>
2024-07-24 19:07:31 +0700

    add comment servive, mapper, and add user in comment response

--- MyNewsWebApp/src/main/java/com/example/newswebapp/Mapper/CommentMapper.java
+++ MyNewsWebApp/src/main/java/com/example/newswebapp/Mapper/CommentMapper.java
@@ -2,7 +2,17 @@ package com.example.newswebapp.Mapper;
 
 import org.springframework.stereotype.Service;
 
+import com.example.newswebapp.Model.Comment;
+import com.example.newswebapp.dto.CommentRequest;
+
 @Service
 public class CommentMapper {
 
+    public Comment toComment(CommentRequest request){
+        return Comment.builder()
+        .id(request.id())
+        .content(request.content())
+        .build();
+    }
 }
+
--- MyNewsWebApp/src/main/java/com/example/newswebapp/Service/CommentService.java
+++ MyNewsWebApp/src/main/java/com/example/newswebapp/Service/CommentService.java
@@ -1,8 +1,36 @@
 package com.example.newswebapp.Service;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.newswebapp.Mapper.CommentMapper;
+import com.example.newswebapp.Model.Comment;
+import com.example.newswebapp.Model.Post;
+import com.example.newswebapp.Model.User;
+import com.example.newswebapp.dto.CommentRequest;
+import com.example.newswebapp.repository.CommentRepository;
+import com.example.newswebapp.repository.PostRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class CommentService {
 
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final CommentMapper commentMapper;
+    
+
+    public Integer save(CommentRequest request, Authentication connectedUser, Long postId) {
+        Post post = postRepository.findById(postId)
+        .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND ,"Post not found"));
+        User user = ((User) connectedUser.getPrincipal());
+        Comment comment = commentMapper.toComment(request);
+        comment.setUser(user);
+        return commentRepository.save(comment).getId();
+}
 }
